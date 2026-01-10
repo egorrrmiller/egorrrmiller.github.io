175096e90701a549e794c344f31e50c6415d0604
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-10 15:57:29 +0300

    Update change_head_menu.js

--- change_head_menu.js
+++ change_head_menu.js
@@ -2,40 +2,11 @@
     'use strict';
 
     function initCustomHead() {
-        // Создаем кнопку перезагрузки (если её нет)
-        if (!$('#reboot-plugin').length) {
-            var icon = '<svg fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" stroke-width="0.48"><path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z" fill="currentColor"></path></svg>';
-            var button = $('<div id="reboot-plugin" class="head__action selector">' + icon + '</div>');
-            
-            button.on('hover:enter hover:click hover:touch', function() {
-                window.location.reload();
-            });
-
-            $('#app > div.head > div > div.head__actions').append(button);
-        }
-
-        // Функция применения видимости
-        function applyVisibility() {
-            // Кнопка перезагрузки
-            if (Lampa.Storage.get('head_reboot', 'true')) $('#reboot-plugin').removeClass('hide');
-            else $('#reboot-plugin').addClass('hide');
-
-            // Уведомления
-            if (Lampa.Storage.get('head_notifications', 'true')) $('.head__action--notifications').removeClass('hide');
-            else $('.head__action--notifications').addClass('hide');
-
-            // Профиль
-            if (Lampa.Storage.get('head_profile', 'true')) $('.head__action--profile').removeClass('hide');
-            else $('.head__action--profile').addClass('hide');
-
-            if (window.Lampa && Lampa.Head) Lampa.Head.update();
-        }
-
-        // Регистрируем параметры в Storage
+        // --- 1. ПАРАМЕТРЫ В STORAGE ---
         Lampa.SettingsApi.addParam({
             component: 'head_settings',
             param: { name: 'head_reboot', type: 'trigger', default: true },
-            field: { name: 'Кнопка перезагрузки', description: 'Показать/скрыть кнопку перезагрузки' },
+            field: { name: 'Кнопка перезагрузки', description: 'Показать/скрыть иконку перезагрузки' },
             onChange: applyVisibility
         });
 
@@ -53,7 +24,33 @@
             onChange: applyVisibility
         });
 
-        // Добавляем пункт "Верхняя панель" в список настроек
+        // --- 2. ЛОГИКА ВИДИМОСТИ ---
+        function applyVisibility() {
+            // Перезагрузка
+            if (Lampa.Storage.field('head_reboot')) {
+                if (!$('#RELOAD_BTN').length) {
+                    var icon = '<div id="RELOAD_BTN" class="head__action selector"><svg fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" stroke-width="0.48"><path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z" fill="currentColor"></path></svg></div>';
+                    $('#app > div.head > div > div.head__actions').append(icon);
+                    $('#RELOAD_BTN').on('hover:enter hover:click hover:touch', function() {
+                        location.reload();
+                    });
+                }
+                $('#RELOAD_BTN').removeClass('hide');
+            } else {
+                $('#RELOAD_BTN').addClass('hide');
+            }
+
+            // Уведомления и Профиль
+            if (Lampa.Storage.field('head_notifications')) $('.head__action--notifications').removeClass('hide');
+            else $('.head__action--notifications').addClass('hide');
+
+            if (Lampa.Storage.field('head_profile')) $('.head__action--profile').removeClass('hide');
+            else $('.head__action--profile').addClass('hide');
+
+            if (window.Lampa && Lampa.Head) Lampa.Head.update();
+        }
+
+        // --- 3. ВНЕДРЕНИЕ В МЕНЮ НАСТРОЕК ---
         Lampa.Settings.listener.follow('open', function (e) {
             if (e.name === 'main') {
                 var item = $('<div class="settings-folder selector" data-component="head_settings">' +
@@ -62,8 +59,21 @@
                 '</div>');
 
                 item.on('hover:enter', function () {
-                    Lampa.Settings.main().render().find('.scroll').empty(); // Очищаем экран настроек
-                    Lampa.Settings.main().render().find('.scroll').append(Lampa.SettingsApi.render({component: 'head_settings'})); // Рисуем наши параметры
+                    // Отрисовка подменю
+                    var body = Lampa.Settings.main().render();
+                    body.find('.scroll').empty().append(Lampa.SettingsApi.render({component: 'head_settings'}));
+                    
+                    // Передаем управление контроллеру (чтобы работал пульт)
+                    Lampa.Controller.add('settings_head', {
+                        toggle: function () {
+                            Lampa.Controller.collectionSet(body);
+                            Lampa.Controller.follow('settings_head');
+                        },
+                        up: function () { Lampa.Navigator.move('up'); },
+                        down: function () { Lampa.Navigator.move('down'); },
+                        back: function () { Lampa.Settings.main().render(); }
+                    });
+                    Lampa.Controller.toggle('settings_head');
                 });
 
                 e.body.find('.scroll').append(item);
