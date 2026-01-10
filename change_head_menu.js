817a086b12aad35306f7c49c192c6316c85ca1e2
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-10 16:30:24 +0300

    Update change_head_menu.js

--- change_head_menu.js
+++ change_head_menu.js
@@ -5,7 +5,7 @@
     Lampa.SettingsApi.addComponent({
         component: 'header_ui_tweaks',
         name: 'Верхняя панель',
-        icon: '<svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#fff"><path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z"/></svg>'
+        icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 6H20M4 12H20M4 18H20" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'
     });
 
     // 2. Добавляем параметры
@@ -19,74 +19,65 @@
     Lampa.SettingsApi.addParam({
         component: 'header_ui_tweaks',
         param: { name: 'head_fullscreen_enabled', type: 'trigger', default: true },
-        field: { name: 'Полноэкранный режим', description: 'Показать иконку развертывания (full--screen)' },
+        field: { name: 'Полноэкранный режим', description: 'Показать иконку развертывания' },
         onChange: applySettings
     });
 
     Lampa.SettingsApi.addParam({
         component: 'header_ui_tweaks',
         param: { name: 'head_notify_enabled', type: 'trigger', default: true },
-        field: { name: 'Уведомления', description: 'Показать колокольчик (notice--icon)' },
+        field: { name: 'Уведомления', description: 'Показать колокольчик' },
         onChange: applySettings
     });
 
     Lampa.SettingsApi.addParam({
         component: 'header_ui_tweaks',
         param: { name: 'head_profile_enabled', type: 'trigger', default: true },
-        field: { name: 'Профиль', description: 'Показать иконку входа (open--profile)' },
+        field: { name: 'Профиль', description: 'Показать иконку входа' },
         onChange: applySettings
     });
 
-    // 3. Основная логика работы
+    // 3. Основная логика
     function applySettings() {
-        // --- Кнопка Перезагрузки (Кастомная) ---
         var reboot_btn = $('#custom_reboot_icon');
         if (Lampa.Storage.field('head_reboot_enabled')) {
             if (!reboot_btn.length) {
                 var html = '<div id="custom_reboot_icon" class="head__action selector">' +
-                    '<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19.9383 13C19.9723 12.671 20 12.3394 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20C12.3394 20 12.671 19.9723 13 19.9383M13 19.9383V16M13 19.9383L15 17.9383M13 19.9383L11 17.9383" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>' +
+                    '<svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" style="fill: currentColor; display: block;">' +
+                    '<path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/>' +
+                    '<path d="M0 0h24v24H0z" fill="none"/>' +
+                    '</svg>' +
                     '</div>';
+                
                 $('.head .head__actions').prepend(html);
-                $('#custom_reboot_icon').on('hover:enter click', function () { window.location.reload(); });
+                
+                reboot_btn = $('#custom_reboot_icon');
+                reboot_btn.on('hover:enter click', function () { 
+                    window.location.reload(); 
+                });
             }
-            $('#custom_reboot_icon').removeClass('hide');
+            reboot_btn.removeClass('hide');
         } else {
-            $('#custom_reboot_icon').addClass('hide');
+            reboot_btn.addClass('hide');
         }
 
-        // --- Управление штатными кнопками по вашим селекторам ---
-        
-        // Полноэкранный режим (full--screen)
-        if (Lampa.Storage.field('head_fullscreen_enabled')) {
-            $('.full--screen').removeClass('hide');
-        } else {
-            $('.full--screen').addClass('hide');
-        }
+        if (Lampa.Storage.field('head_fullscreen_enabled')) $('.full--screen').removeClass('hide');
+        else $('.full--screen').addClass('hide');
 
-        // Уведомления (notice--icon)
-        if (Lampa.Storage.field('head_notify_enabled')) {
-            $('.notice--icon').removeClass('hide');
-        } else {
-            $('.notice--icon').addClass('hide');
-        }
+        if (Lampa.Storage.field('head_notify_enabled')) $('.notice--icon').removeClass('hide');
+        else $('.notice--icon').addClass('hide');
 
-        // Профиль (open--profile)
-        if (Lampa.Storage.field('head_profile_enabled')) {
-            $('.open--profile').removeClass('hide');
-        } else {
-            $('.open--profile').addClass('hide');
-        }
+        if (Lampa.Storage.field('head_profile_enabled')) $('.open--profile').removeClass('hide');
+        else $('.open--profile').addClass('hide');
 
-        // Синхронизируем навигацию (чтобы пульт не перескакивал через скрытые кнопки)
         if(window.Lampa && Lampa.Head) Lampa.Head.update();
     }
 
-    // 4. Запуск плагина
+    // 4. Запуск
     if (window.appready) applySettings();
     else {
         Lampa.Listener.follow('app', function (e) {
             if (e.type == 'ready') applySettings();
         });
     }
-
 })();
