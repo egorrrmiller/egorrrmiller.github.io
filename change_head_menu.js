617ca086936a6c048d9a1a8b80dfa5517c480514
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-10 16:51:35 +0300

    Update change_head_menu.js

--- change_head_menu.js
+++ change_head_menu.js
@@ -1,14 +1,14 @@
 (function () {
     'use strict';
 
-    // 1. Создаем раздел в меню настроек
+    // Регистрация раздела в меню настроек
     Lampa.SettingsApi.addComponent({
         component: 'header_ui_tweaks',
         name: 'Верхняя панель',
         icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 6H20M4 12H20M4 18H20" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'
     });
 
-    // 2. Добавляем параметры (настройки)
+    // Параметр: Кнопка перезагрузки
     Lampa.SettingsApi.addParam({
         component: 'header_ui_tweaks',
         param: { name: 'head_reboot_enabled', type: 'trigger', default: true },
@@ -16,13 +16,15 @@
         onChange: applySettings
     });
 
+    // Параметр: Поиск
     Lampa.SettingsApi.addParam({
         component: 'header_ui_tweaks',
         param: { name: 'head_search_enabled', type: 'trigger', default: true },
-        field: { name: 'Поиск', description: 'Показать иконку поиска (open--search)' },
+        field: { name: 'Поиск', description: 'Показать иконку поиска' },
         onChange: applySettings
     });
 
+    // Параметр: Полноэкранный режим
     Lampa.SettingsApi.addParam({
         component: 'header_ui_tweaks',
         param: { name: 'head_fullscreen_enabled', type: 'trigger', default: true },
@@ -30,6 +32,7 @@
         onChange: applySettings
     });
 
+    // Параметр: Уведомления
     Lampa.SettingsApi.addParam({
         component: 'header_ui_tweaks',
         param: { name: 'head_notify_enabled', type: 'trigger', default: true },
@@ -37,6 +40,7 @@
         onChange: applySettings
     });
 
+    // Параметр: Профиль
     Lampa.SettingsApi.addParam({
         component: 'header_ui_tweaks',
         param: { name: 'head_profile_enabled', type: 'trigger', default: true },
@@ -44,53 +48,48 @@
         onChange: applySettings
     });
 
-    // 3. Основная логика применения настроек
     function applySettings() {
+        
+        // Блок: Кнопка перезагрузки
         var reboot_btn = $('#custom_reboot_icon');
         if (Lampa.Storage.field('head_reboot_enabled')) {
             if (!reboot_btn.length) {
-                var html = '<div id="custom_reboot_icon" class="head__action selector">' +
-                    '<svg fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" stroke-width="0.48" style="width: 1.5em; height: 1.5em;">' +
-                    '<g id="SVGRepo_bgCarrier" stroke-width="0"></g>' +
-                    '<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>' +
-                    '<g id="SVGRepo_iconCarrier">' +
+                var html = $('<div id="custom_reboot_icon" class="head__action selector">' +
+                    '<svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="width: 1.5em; height: 1.5em;">' +
                     '<path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z" fill="currentColor"></path>' +
-                    '</g></svg>' +
-                    '</div>';
-                
+                    '</svg></div>');
                 $('.head .head__actions').append(html);
-                
-                reboot_btn = $('#custom_reboot_icon');
-                reboot_btn.on('hover:enter click', function () { 
-                    window.location.reload(); 
-                });
+                html.on('hover:enter click', function () { window.location.reload(); });
+                reboot_btn = html;
             }
             reboot_btn.removeClass('hide');
         } else {
             reboot_btn.addClass('hide');
         }
-        
-        // Поиск
+
+        // Блок: Поиск
         if (Lampa.Storage.field('head_search_enabled')) $('.open--search').removeClass('hide');
         else $('.open--search').addClass('hide');
 
-        // Полноэкранный режим
+        // Блок: Полноэкранный режим
         if (Lampa.Storage.field('head_fullscreen_enabled')) $('.full--screen').removeClass('hide');
         else $('.full--screen').addClass('hide');
 
-        // Уведомления
+        // Блок: Уведомления
         if (Lampa.Storage.field('head_notify_enabled')) $('.notice--icon').removeClass('hide');
         else $('.notice--icon').addClass('hide');
 
-        // Профиль
+        // Блок: Профиль
         if (Lampa.Storage.field('head_profile_enabled')) $('.open--profile').removeClass('hide');
         else $('.open--profile').addClass('hide');
 
-        // Обновляем Head, чтобы Lampa пересчитала доступные кнопки для пульта
-        if(window.Lampa && Lampa.Head) Lampa.Head.update();
+        // Блок: Обновление навигации
+        if (window.Lampa && Lampa.Head && typeof Lampa.Head.update === 'function') {
+            Lampa.Head.update();
+        }
     }
 
-    // 4. Инициализация
+    // Инициализация плагина
     if (window.appready) applySettings();
     else {
         Lampa.Listener.follow('app', function (e) {
