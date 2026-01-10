a475cc7de588557b1330067c6011648896657c05
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-10 19:42:21 +0300

    Update ratings.js

--- ratings.js
+++ ratings.js
@@ -1,5 +1,6 @@
 (function () {
     'use strict';
+    Lampa.Params.select('kp_unofficial_token','','');
 
     // Стили для увеличенных рейтингов
     function addStyles() {
@@ -34,15 +35,13 @@
     /**
      * ИНИЦИАЛИЗАЦИЯ НАСТРОЕК ЧЕРЕЗ СТАНДАРТНОЕ API
      */
-    function setupSettings() {
+    function setupSettings() {        
         // Регистрируем компонент
         Lampa.SettingsApi.addComponent({
             component: 'ratings_tweaks',
             name: 'Рейтинги',
             icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z" fill="white"/></svg>'
         });
-
-        Lampa.Params.select('kp_unofficial_token','','')  
         
         // Твой код для addParam
         Lampa.SettingsApi.addParam({
@@ -50,7 +49,7 @@
             param: {
                 name: 'kp_unofficial_token',
                 type: 'input',
-                default: ``, // Поставил рабочий как дефолт
+                default: '', // Поставил рабочий как дефолт
                 placeholder: 'Введите ключ...'
             },
             field: {
