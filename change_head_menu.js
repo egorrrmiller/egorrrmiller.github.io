50d735fb222398187179ae2100585fc639fc1dfc
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-10 16:27:25 +0300

    Update change_head_menu.js

--- change_head_menu.js
+++ change_head_menu.js
@@ -44,7 +44,7 @@
         if (Lampa.Storage.field('head_reboot_enabled')) {
             if (!reboot_btn.length) {
                 var html = '<div id="custom_reboot_icon" class="head__action selector">' +
-                    '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/><path d="M0 0h24v24H0z" fill="none"/></svg>' +
+                    '<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19.9383 13C19.9723 12.671 20 12.3394 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20C12.3394 20 12.671 19.9723 13 19.9383M13 19.9383V16M13 19.9383L15 17.9383M13 19.9383L11 17.9383" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>' +
                     '</div>';
                 $('.head .head__actions').prepend(html);
                 $('#custom_reboot_icon').on('hover:enter click', function () { window.location.reload(); });
