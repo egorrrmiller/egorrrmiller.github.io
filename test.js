3cdfa158da4beb2668259a34bfba96cb88085ce9
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-11 18:59:33 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
+/*
+* 1.
+*
+* */
+
 (function () {
     let tmdb_cache = {};
 
     // 1. Слушаем событие рендера каждого файла в списке торрентов
     Lampa.Listener.follow('torrent_file', (e) => {
+
+        if (e.type === 'render'){
+            e.item.find('.torrent-serial__title').text('тест тест тест');
+        }
+
         console.log(e);
     });
 })();
