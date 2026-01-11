2a6c9adaf761f44385c0eb34c6dae690ef364036
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-11 13:38:41 +0300

    Update fix_lampac_cub_auth.js

--- fix_lampac_cub_auth.js
+++ fix_lampac_cub_auth.js
@@ -4,40 +4,45 @@
     function inject() {
         var keyboard = $('.simple-keyboard');
         
-        if (keyboard.length && !keyboard.find('.simple-keyboard-buttons').length) {
-            // Подготавливаем окружение для твоего кода
-            var _this = Lampa.Input.active();
-            if (!_this) return; // Ждем, пока Lampa создаст объект
-
-            var input = keyboard.find('#orsay-keyboard');
+        // Если клавиатура есть, а кнопок еще нет
+        if (keyboard.length && !keyboard.find('.my-added-buttons').length) {
+            
             var Lang = Lampa.Lang;
             var Controller = Lampa.Controller;
 
-            // --- ТВОЙ КОД (БЕЗ ИЗМЕНЕНИЙ) ---
-            var buttons = $('<div class="simple-keyboard-buttons"><div class="simple-keyboard-buttons__enter">' + Lang.translate('ready') + '</div><div class="simple-keyboard-buttons__cancel">' + Lang.translate('cancel') + '</div></div>');
+            // 1. Создаем кнопки (ТВОЙ КОД)
+            // Добавим класс my-added-buttons, чтобы избежать повторов
+            var buttons = $('<div class="simple-keyboard-buttons my-added-buttons"><div class="simple-keyboard-buttons__enter">' + Lang.translate('ready') + '</div><div class="simple-keyboard-buttons__cancel">' + Lang.translate('cancel') + '</div></div>');
             
+            // 2. Вешаем обработчики (ТВОЙ КОД)
+            // Переменные _this и input определяются ВНУТРИ клика, чтобы не вешать окно при загрузке
             buttons.find('.simple-keyboard-buttons__enter').on('click', function () {
+                var _this = Lampa.Input.active();
+                var input = $('.simple-keyboard').find('#orsay-keyboard');
+                
                 input.blur();
 
                 _this.listener.send('enter');
             });
-
+            
             buttons.find('.simple-keyboard-buttons__cancel').on('click', function () {
+                var _this = Lampa.Input.active();
+                
                 _this.value('');
 
                 Controller.back();
             });
 
+            // 3. Вставляем (ТВОЙ КОД)
             $('.simple-keyboard').append(buttons);
-            // --- КОНЕЦ ТВОЕГО КОДА ---
 
-            // Добавляем селекторы, чтобы Lampa видела кнопки пультом/мышью
+            // Добавляем селекторы для работы навигации
             buttons.find('div').addClass('selector');
             if (Controller.update) Controller.update();
         }
     }
 
-    // Слушатель для отрисовки
+    // Слушатель изменений DOM
     var observer = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             if (mutation.addedNodes.length) inject();
@@ -49,7 +54,7 @@
             observer.observe(document.body, { childList: true, subtree: true });
             inject();
         } else {
-            setTimeout(start, 100);
+            setTimeout(start, 200);
         }
     }
 
