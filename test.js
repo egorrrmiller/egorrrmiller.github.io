a4c8e8332a4fa51b359ed091a170f136e239148b
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-11 21:14:51 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,100 +1,79 @@
-(function () {
-    function getDirectTMDBImage(path, size) {
-        return `https://imagetmdb.com/t/p/${size}/${path}`;
-    }
-
-    try {
-        let seasonCache = {};
-
-        // сюда попадем столько раз, сколько файлов в торренте
-        Lampa.Listener.follow('torrent_file', (e) => {
-            if (e.type === 'list_open') {
-                seasonCache = {}; // Очистка при открытии нового списка
-            }
-            else if (e.type === 'render') {
-                console.log('e', e)
-
-                let html = e.item;
-                let data = e.element;
-                let movie = e.params.movie;
-
-                let episode = data.episode; // каждый раз разный эпизод.
-                let seasonNum = data.season || 1;
-                let cacheKey = `${movie.id}_s${seasonNum}`;
-
-                let allFilesCount = e.items;
-
-                if (!movie || !movie.id || !data.title || !allFilesCount) return;
-
-                let fileName = data.folder_name || data.path;
-                let checkPart = fileName.match(/(?:часть|part|pt?\.?)\s*(\d+)/i);
-                console.log('checkPart', checkPart);
-
-                if (checkPart) {
-
-                    let applyEpisodeData = (episodes) => {
-                        console.log('episodes', episodes);
-                        let totalInTorrent = allFilesCount.length;
-                        let totalInTMDB = episodes.length;
-
-                        /* Логика: если в торренте файлов меньше, чем в сезоне,
-                           считаем, что это "хвост" сезона.
-                           Пример: TMDB=20, Торрент=5.
-                           Часть 1 торрента = 16-я серия TMDB (20 - 5 + 1)
-                        */
-                        let offset = Math.max(0, totalInTMDB - totalInTorrent);
-                        console.log('offset', offset);
-
-                        let targetEpisodeNumber = offset + episode;
-                        console.log('targetEpisodeNumber', targetEpisodeNumber);
-
-                        let targetEpisode = episodes.find(ep => ep.episode_number === targetEpisodeNumber);
-
-                        if (targetEpisode) {
-                            // Обновление UI
-                            html.find('.torrent-serial__title').text(targetEpisode.name);
-
-                            if (targetEpisode.air_date) {
-                                let date = Lampa.Utils.parseTime(targetEpisode.air_date).full;
-                                html.find('.torrent-serial__line span:last').text(`Выход - ${date}`);
-                            }
-
-                            let img;
-                            if (targetEpisode.still_path) {
-                                img = getDirectTMDBImage(targetEpisode.still_path, 'w300');
-                                console.info('img', img)
-
-                                html.find('.torrent-serial__img').attr('src', img);
-                            }
-
-                            // Обновление объекта данных (для плеера)
-                            data.title = targetEpisode.name;
-                            data.fname = targetEpisode.name;
-                            data.img = img;
-                        }
-                    };
-
-                    // Проверка кэша или запрос к API
-                    if (seasonCache[cacheKey]) {
-                        applyEpisodeData(seasonCache[cacheKey]);
-                    } else {
-                        Lampa.Api.sources.tmdb.get(`tv/${movie.id}/season/${seasonNum}?language=ru-RU`, {}, (tmdbData) => {
-                            if (tmdbData && (tmdbData.episodes_original)) {
-                                let eps = tmdbData.episodes_original;
-                                seasonCache[cacheKey] = eps;
-                                applyEpisodeData(eps);
-
-                                console.log(tmdbData);
-                            }
-                        }, (error) => {
-                            console.error('TMDB API Error:', error);
-                        });
-                    }
-                }
-            }
-        });
-    }
-    catch (error) {
-        console.error('Plugin Error: ', error);
-    }
+(function() {  
+    let seasonCache = {};  
+    let loadingPromises = {};  
+  
+    function preloadSeasonData(movieId, seasonNum) {  
+        const cacheKey = `${movieId}_s${seasonNum}`;  
+          
+        if (seasonCache[cacheKey]) {  
+            return Promise.resolve(seasonCache[cacheKey]);  
+        }  
+          
+        if (loadingPromises[cacheKey]) {  
+            return loadingPromises[cacheKey];  
+        }  
+          
+        loadingPromises[cacheKey] = new Promise((resolve) => {  
+            Lampa.Api.sources.tmdb.get(`tv/${movieId}/season/${seasonNum}?language=ru-RU`, {}, (tmdbData) => {  
+                if (tmdbData && tmdbData.episodes_original) {  
+                    seasonCache[cacheKey] = tmdbData.episodes_original;  
+                    resolve(seasonCache[cacheKey]);  
+                } else {  
+                    resolve([]);  
+                }  
+            });  
+        });  
+          
+        return loadingPromises[cacheKey];  
+    }  
+  
+    Lampa.Listener.follow('torrent_file', async (e) => {  
+        if (e.type === 'list_open') {  
+            seasonCache = {};  
+            loadingPromises = {};  
+              
+            // Предзагружаем данные для всего сезона  
+            const movie = e.params.movie;  
+            if (movie && movie.id) {  
+                // Определяем сезон из первого элемента  
+                const firstItem = e.items[0];  
+                if (firstItem) {  
+                    const info = Torserver.parse({  
+                        movie: movie,  
+                        files: e.items,  
+                        filename: firstItem.path_human,  
+                        path: firstItem.path,  
+                    });  
+                      
+                    if (info.season) {  
+                        await preloadSeasonData(movie.id, info.season);  
+                    }  
+                }  
+            }  
+        }  
+        else if (e.type === 'render') {  
+            const item = e.item;  
+            const data = e.element;  
+            const movie = e.params.movie;  
+              
+            if (!movie || !movie.id || !data.title) return;  
+              
+            const fileName = data.folder_name || data.path;  
+            const checkPart = fileName.match(/(?:часть|part|pt?\.?)\s*(\d+)/i);  
+              
+            if (checkPart) {  
+                const seasonNum = data.season || 1;  
+                const cacheKey = `${movie.id}_s${seasonNum}`;  
+                  
+                // Данные уже должны быть в кэше  
+                if (seasonCache[cacheKey]) {  
+                    applyEpisodeData(seasonCache[cacheKey]);  
+                }  
+            }  
+        }  
+    });  
+  
+    function applyEpisodeData(episodes) {  
+        // Ваша логика применения данных...  
+    }  
 })();
