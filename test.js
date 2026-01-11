580100d28bcf0174c1ed9d03302c5199fee734dc
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-11 18:41:31 +0300

    Update test.js

--- test.js
+++ test.js
@@ -41,5 +41,49 @@
                 }  
             }  
         }  
-    });  
+    }); 
+
+    function applyChanges(item, episodes, epNum, seasonNum) {
+        const epData = episodes.find(e => e.episode_number == epNum);
+        if (!epData) return;
+
+        item.attr('data-redrawn', 'true');
+
+        // Формируем новый внутренний HTML, идентичный структуре Lampa, но с нашими данными
+        const img = epData.still_path ? Lampa.TMDB.image(epData.still_path, 'w500') : './img/img_broken.svg';
+        const date = epData.air_date ? Lampa.Utils.parseDate(epData.air_date).format('DD MMMM YYYY') : '--';
+
+        // Полностью перерисовываем внутреннюю структуру .torrent-serial
+        item.html(`
+            <img src="${img}" class="torrent-serial__img loaded">
+            <div class="torrent-serial__content">
+                <div class="torrent-serial__body">
+                    <div class="torrent-serial__title">${epData.name}</div>
+                    <div class="torrent-serial__line">
+                        <span>Сезон - <b>${seasonNum}</b></span>
+                        <span>Выход - ${date}</span>
+                    </div>
+                </div>
+                <div class="torrent-serial__detail">
+                    <div class="torrent-serial__size">${item.find('.torrent-serial__size').text() || ''}</div>
+                    <div class="torrent-serial__exe">${item.find('.torrent-serial__exe').text() || ''}</div>
+                </div>
+                <div class="torrent-serial__clear"></div>
+            </div>
+            <div class="torrent-serial__episode">${epNum}</div>
+        `);
+        
+        // Добавляем таймлайн, если он был (прогресс просмотра)
+        if (item.data('hash')) {
+            const hash = item.data('hash');
+            const timeline = Lampa.Timeline.view(hash);
+            if (timeline && timeline.percent > 0) {
+                item.find('.torrent-serial__content').append(`
+                    <div class="time-line">
+                        <div style="width: ${timeline.percent}%"></div>
+                    </div>
+                `);
+            }
+        }
+    }
 })();
