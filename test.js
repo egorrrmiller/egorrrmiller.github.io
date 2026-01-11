f435200eb9dffd7fd864be13b4da2b29f05f870b
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-01-11 19:18:13 +0300

    Update test.js

--- test.js
+++ test.js
@@ -6,22 +6,73 @@
 (function () {
 
     try {
-        const regex = /(?:часть|part|pt?\.?)\s*(\d+)/i;
-
-        const activity = Lampa.Activity.active();
-
-        const card = activity.movie;
-
-
-        console.log(card)
+        let seasonCache = {};
 
         Lampa.Listener.follow('torrent_file', (e) => {
-
-            if (e.type === 'render'){
-                e.item.find('.torrent-serial__title').text('тест тест тест');
+            if (e.type === 'list_open') {
+                seasonCache = {};
             }
+            else if (e.type === 'render') {
+                const item = e.item;
+                const data = e.element;
+                const movie = e.params.movie;
 
-            console.log(e);
+                if (!movie || !movie.id || !data.title) return;
+
+                const match = data.title.match(/(?:часть|part|pt?\.?)\s*(\d+)/i);
+
+                if (match) {
+                    const partNumber = parseInt(match[1]);
+                    const seasonNum = data.season || 1;
+                    const cacheKey = `${movie.id}_s${seasonNum}`;
+
+                    const applyEpisodeData = (episodes) => {
+                        // Вычисляем номер эпизода: общее кол-во - номер части  
+                        const totalEpisodes = episodes.length;
+                        const targetEpisodeNumber = totalEpisodes - partNumber;
+                        
+                        console.log('totalEpisodes', totalEpisodes);
+                        console.log('partNumber', partNumber);
+
+                        // Ищем эпизод с вычисленным номером  
+                        const targetEpisode = episodes.find(ep => ep.episode_number === targetEpisodeNumber);
+
+                        if (targetEpisode) {
+                            // Обновляем DOM  
+                            item.find('.torrent-serial__title').text(targetEpisode.name);
+
+                            if (targetEpisode.air_date) {
+                                const date = Lampa.Utils.parseTime(targetEpisode.air_date).full;
+                                item.find('.torrent-serial__line span:last').text(`Выход - ${date}`);
+                            }
+
+                            if (targetEpisode.still_path) {
+                                const img = Lampa.TMDB.image(targetEpisode.still_path, 'w500');
+                                item.find('.torrent-serial__img').attr('src', img);
+                            }
+
+                            // Обновляем данные  
+                            data.title = targetEpisode.name;
+                            data.fname = targetEpisode.name;
+                            data.air_date = targetEpisode.air_date;
+                            data.img = img;
+
+                            console.log(`Часть ${partNumber} → Эпизод ${targetEpisodeNumber}: ${targetEpisode.name}`);
+                        }
+                    };
+
+                    if (seasonCache[cacheKey]) {
+                        applyEpisodeData(seasonCache[cacheKey]);
+                    } else {
+                        Lampa.TMDB.get(`tv/${movie.id}/season/${seasonNum}?language=ru-RU`, {}, (tmdbData) => {
+                            if (tmdbData && tmdbData.episodes) {
+                                seasonCache[cacheKey] = tmdbData.episodes;
+                                applyEpisodeData(tmdbData.episodes);
+                            }
+                        });
+                    }
+                }
+            }
         });
     }
     catch (error) {
