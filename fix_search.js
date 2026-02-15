2580585b1eaa3e011bdb9107b4306274f0724e6a
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-02-15 23:52:18 +0300

    Update fix_search.js

--- fix_search.js
+++ fix_search.js
@@ -1,52 +1,62 @@
 (function () {
     'use strict';
 
+    // Добавляем настройку
+    Lampa.Settings.listener.follow('open', function (e) {
+        if (e.name == 'parser') {
+            var item = $(`
+                <div class="settings-param selector" data-type="toggle" data-name="parser_use_tmdb_id">
+                    <div class="settings-param__name">Поиск по TMDB ID</div>
+                    <div class="settings-param__value"></div>
+                    <div class="settings-param__descr">Добавлять ID TMDB к запросу поиска торрентов</div>
+                </div>
+            `);
+
+            var target = e.body.find('[data-name="parser_torrent_type"]');
+            if (target.length) {
+                target.after(item);
+            } else {
+                e.body.find('.settings-param').first().before(item);
+            }
+
+            Lampa.Params.bind(item);
+        }
+    });
+
     Lampa.Listener.follow('request_before', function (e) {
         // Проверяем, что это запрос к Jackett
         if (e.params.url && e.params.url.indexOf('/api/v2.0/indexers/') !== -1 && e.params.url.indexOf('/results') !== -1) {
-            var activity = Lampa.Activity.active();
-            var movie = null;
+            
+            // Убираем параметр year из запроса
+            e.params.url = e.params.url.replace(/([?&])year=[^&]*&?/, '$1').replace(/&$/, '');
 
-            if (activity.component === 'full' && activity.card) movie = activity.card;
-            if (activity.component === 'torrents' && activity.movie) movie = activity.movie;
+            /*
+            // Логика добавления TMDB ID (закомментирована)
+            if (Lampa.Storage.field('parser_use_tmdb_id')) {
+                var activity = Lampa.Activity.active();
+                var movie = null;
 
-            if (movie) {
-                // 1. Добавляем TMDB ID
-                /* if (movie.id) {
+                if (activity.component === 'full' && activity.card) movie = activity.card;
+                if (activity.component === 'torrents' && activity.movie) movie = activity.movie;
+
+                if (movie && movie.id) {
                     var match = e.params.url.match(/title_original=([^&]+)/);
-                    var shouldAddId = false;
-
+                    
                     if (match && match[1]) {
                         var urlTitle = decodeURIComponent(match[1]).toLowerCase();
                         var cardTitle = (movie.original_title || movie.original_name || '').toLowerCase();
 
                         if (urlTitle === cardTitle || cardTitle.indexOf(urlTitle) > -1 || urlTitle.indexOf(cardTitle) > -1) {
-                            shouldAddId = true;
+                            e.params.url = Lampa.Utils.addUrlComponent(e.params.url, 'tmdb=' + movie.id);
+                            e.params.url = Lampa.Utils.addUrlComponent(e.params.url, 'tmdb-search=true');
                         }
                     } else {
-                        shouldAddId = true;
-                    }
-
-                    if (shouldAddId) {
                         e.params.url = Lampa.Utils.addUrlComponent(e.params.url, 'tmdb=' + movie.id);
-                    }
-                } */
-
-                // 2. Фикс поиска (Query = search_one + search_two)
-                if (activity.component === 'torrents' && activity.search_one && activity.search_two) {
-                    var newQuery = activity.search_one + ' ' + activity.search_two;
-                    
-                    // Обновляем Query в URL
-                    e.params.url = e.params.url.replace(/([?&]Query=)([^&]*)/, function(match, prefix, oldValue) {
-                        return prefix + encodeURIComponent(newQuery);
-                    });
-
-                    // Обновляем отображаемый поисковый запрос в активности, если нужно
-                    if (activity.search !== newQuery) {
-                        activity.search = newQuery;
+                        e.params.url = Lampa.Utils.addUrlComponent(e.params.url, 'tmdb-search=true');
                     }
                 }
             }
+            */
         }
     });
 })();
