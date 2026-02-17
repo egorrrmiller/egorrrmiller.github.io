6aa22aff45d47601930c6ada2425683ca6d8f9d7
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-02-17 14:33:18 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,236 +1,100 @@
 (function () {
     'use strict';
 
-    var waitLampa = setInterval(function(){
-        if(window.Lampa && window.Lampa.Listener){
-            clearInterval(waitLampa);
-            init();
+    // Перехват запросов
+    Lampa.Listener.follow('request_before', function (e) {
+        // Проверяем, что это запрос к Jackett
+        if (e.params.url && e.params.url.indexOf('/api/v2.0/indexers/') !== -1 && e.params.url.indexOf('/results') !== -1) {
+            // Убираем year
+            e.params.url = e.params.url.replace(/([?&])year=[^&]*&?/, '$1').replace(/&$/, '');
         }
-    }, 200);
+    });
 
-    function init(){
-        console.log('TT: Plugin init');
-
-        var style = document.createElement('style');
-        style.innerHTML = '.torrent-serial__title, .torrent-serial__line, .torrent-serial__img { transition: opacity 0.3s ease; } .ts-hidden { opacity: 0; }';
-        document.head.appendChild(style);
-
-        function getDirectTMDBImage(path, size) {
-            return 'https://imagetmdb.com/t/p/' + size + '/' + path;
-        }
-
-        var seasonCache = {};
-        var torrentSeason = null;
-        var torrentOffset = 0;
-        var isPart2Global = false;
-
-        Lampa.Listener.follow('torrent_file', function (e) {
-            if (e.type === 'list_open') {
-                console.log('TT: list_open', e);
-                seasonCache = {};
-                torrentSeason = null;
-                torrentOffset = 0;
-                isPart2Global = false;
-
-                var activity = Lampa.Activity.active();
-                if (activity && activity.component === 'torrents' && activity.object && activity.object.title) {
-                    var title = activity.object.title;
-                    console.log('TT: Torrent Title:', title);
+    // Добавляем кнопку перезагрузки (Force Search)
+    Lampa.Listener.follow('activity', function (e) {
+        if (e.type === 'start' && e.component === 'torrents') {
+            var waitFilter = setInterval(function(){
+                var filter = $('.torrent-filter');
+                if (filter.length) {
+                    clearInterval(waitFilter);
                     
-                    var matchSeason = title.match(/(?:season|сезон|s)\s*:?\s*(\d+)/i);
-                    if (matchSeason && matchSeason[1]) {
-                        torrentSeason = parseInt(matchSeason[1]);
-                        console.log('TT: Detected season:', torrentSeason);
-                    }
+                    if (filter.find('.filter--reload').length) return;
 
-                    var matchOffset = title.match(/(?:offset|start)\s*:?\s*(\d+)/i);
-                    if (matchOffset && matchOffset[1]) {
-                        var val = parseInt(matchOffset[1]);
-                        if (title.toLowerCase().indexOf('start') > -1) {
-                            torrentOffset = val - 1;
-                        } else {
-                            torrentOffset = val;
-                        }
-                        console.log('TT: Detected offset:', torrentOffset);
-                    }
+                    var btn = $(`
+                        <div class="simple-button simple-button--filter selector filter--reload">
+                            <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="width: 1.5em; height: 1.5em;">
+                                <path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z" fill="currentColor"></path>
+                            </svg>
+                        </div>
+                    `);
 
-                    if (title.match(/(?:part|часть|cour)\s*2/i)) {
-                        isPart2Global = true;
-                        console.log('TT: Detected Part 2 flag (Global)');
-                    }
-                }
-            } else if (e.type === 'render') {
-                var html = e.item;
-                var data = e.element;
-                var movie = e.params.movie;
-                
-                var videoFiles = e.items.filter(function(item){
-                    var ext = (item.path || '').split('.').pop().toLowerCase();
-                    return ['mkv', 'mp4', 'avi', 'mov', 'wmv', 'flv', 'webm', 'm4v'].indexOf(ext) !== -1;
-                });
-                var allFilesCount = videoFiles.length;
+                    filter.find('.filter--sort').before(btn);
 
-                if (!movie || !movie.id) return;
-
-                var seasonNum = data.season || torrentSeason || 1;
-                var episodeNum = parseInt(data.episode);
-
-                var filePath = data.path || '';
-                var fileName = filePath.split('/').pop();
-                
-                var isPart2 = isPart2Global || !!fileName.match(/(?:part|часть|cour)\s*[-.]?\s*2/i);
-
-                var cleanName = fileName.replace(/(?:part|часть|cour)\s*[-.]?\s*2/ig, '');
-                
-                var checkPart = cleanName.match(/(?:e|ep|эп|серия)\s*(\d+)/i) || cleanName.match(/(?:^|\s|\[|\()(\d+)(?:\]|\)|\s|\.)/);
-                
-                if (checkPart && checkPart[1]) {
-                    var parsedNum = parseInt(checkPart[1]);
-                    if (isNaN(episodeNum) || (isPart2 && episodeNum === 2 && parsedNum !== 2)) {
-                         episodeNum = parsedNum;
-                    } else if (isNaN(episodeNum)) {
-                        episodeNum = parsedNum;
-                    }
-                }
-
-                if (!isNaN(episodeNum) && torrentOffset > 0) {
-                    episodeNum += torrentOffset;
-                }
-
-                console.log('TT: Render item', {
-                    fileName: fileName,
-                    cleanName: cleanName,
-                    seasonNum: seasonNum,
-                    episodeNum: episodeNum,
-                    isPart2: isPart2,
-                    allFilesCount: allFilesCount
-                });
-
-                if (isNaN(episodeNum)) return;
-
-                var cacheKey = movie.id + '_s' + seasonNum;
-                var titleElem = html.find('.torrent-serial__title');
-                var lineElem = html.find('.torrent-serial__line');
-                var imgElem = html.find('.torrent-serial__img');
-
-                var applyData = function (episodes) {
-                    console.log('TT: applyData', { episodesCount: episodes ? episodes.length : 0 });
-
-                    if (!episodes || !episodes.length) {
-                        titleElem.removeClass('ts-hidden');
-                        lineElem.removeClass('ts-hidden');
-                        imgElem.removeClass('ts-hidden');
-                        return;
-                    }
-
-                    var targetEpisode = null;
-                    var totalInTMDB = episodes.length;
-
-                    // 1. Part 2 Logic
-                    if (isPart2 && torrentOffset === 0) {
-                        var autoOffset = Math.max(0, totalInTMDB - allFilesCount);
-                        console.log('TT: Part 2 Logic. Total:', totalInTMDB, 'Files:', allFilesCount, 'Offset:', autoOffset);
-                        
-                        if (autoOffset > 0) {
-                            var targetNum = autoOffset + episodeNum;
-                            targetEpisode = episodes.find(function (ep) {
-                                return ep.episode_number === targetNum;
-                            });
-                            if (targetEpisode) console.log('TT: Found via Part 2 Offset:', targetEpisode.name);
-                        }
-                    }
-
-                    // 2. Direct Logic
-                    if (!targetEpisode) {
-                        targetEpisode = episodes.find(function (ep) {
-                            return ep.episode_number === episodeNum;
-                        });
-                        if (targetEpisode) console.log('TT: Found via Direct:', targetEpisode.name);
-                    }
-
-                    // 3. Tail Logic
-                    if (!targetEpisode && torrentOffset === 0) {
-                        var offset = Math.max(0, totalInTMDB - allFilesCount);
-                        if (offset > 0) {
-                             targetEpisode = episodes.find(function (ep) {
-                                return ep.episode_number === offset + episodeNum;
-                            });
-                            if (targetEpisode) console.log('TT: Found via Tail Offset:', targetEpisode.name);
-                        }
-                    }
-
-                    if (targetEpisode) {
-                        titleElem.text(targetEpisode.name);
-
-                        if (targetEpisode.air_date) {
-                            var date = Lampa.Utils.parseTime(targetEpisode.air_date).full;
-                            if(lineElem.find('span').length > 1) lineElem.find('span').last().text('Выход - ' + date);
-                            else lineElem.append('<span>Выход - ' + date + '</span>');
-                        }
-
-                        if (targetEpisode.still_path) {
-                            var img = getDirectTMDBImage(targetEpisode.still_path, 'w300');
-                            if(imgElem.attr('src') !== img){
-                                imgElem.attr('src', img);
-                                data.img = img;
-                            }
-                        }
-
-                        data.title = targetEpisode.name;
-                        data.fname = targetEpisode.name;
-                    }
-
-                    requestAnimationFrame(function() {
-                        titleElem.removeClass('ts-hidden');
-                        lineElem.removeClass('ts-hidden');
-                        imgElem.removeClass('ts-hidden');
-                    });
-                };
-
-                var hasData = false;
-                var episodesData = null;
-
-                if (e.params.seasons && e.params.seasons[seasonNum] && e.params.seasons[seasonNum].episodes) {
-                    episodesData = e.params.seasons[seasonNum].episodes;
-                    // Проверка на неполные данные для Part 2
-                    if (isPart2 && episodesData.length < 20) {
-                        console.log('TT: Data incomplete for Part 2 (only ' + episodesData.length + ' eps). Force reload.');
-                        hasData = false;
-                    } else {
-                        console.log('TT: Data from params');
-                        hasData = true;
-                    }
-                } else if (seasonCache[cacheKey]) {
-                    console.log('TT: Data from cache');
-                    hasData = true;
-                    episodesData = seasonCache[cacheKey];
-                }
-
-                if (hasData) {
-                    applyData(episodesData);
-                } else {
-                    console.log('TT: Fetching data for season', seasonNum);
-                    titleElem.addClass('ts-hidden');
-                    lineElem.addClass('ts-hidden');
-                    imgElem.addClass('ts-hidden');
-
-                    // Запрашиваем данные. Если на русском мало серий, можно попробовать fallback на английский,
-                    // но пока просто перезапросим, вдруг кэш протух.
-                    Lampa.Api.sources.tmdb.get('tv/' + movie.id + '/season/' + seasonNum + '?language=' + Lampa.Storage.get('language','ru'), {}, function (tmdbData) {
-                        console.log('TT: API success', tmdbData);
-                        if (tmdbData && (tmdbData.episodes || tmdbData.episodes_original)) {
-                            var eps = tmdbData.episodes || tmdbData.episodes_original;
-                            seasonCache[cacheKey] = eps;
-                            applyData(eps);
-                        } else {
-                            applyData(null);
-                        }
-                    }, function (error) {
-                        console.log('TT: API error', error);
-                        applyData(null);
+                    btn.on('hover:enter', function () {
+                        forceSearch();
                     });
                 }
+            }, 200);
+        }
+    });
+
+    function forceSearch() {
+        var activity = Lampa.Activity.active();
+        var movie = activity.movie;
+        var query = activity.search;
+
+        var url = Lampa.Storage.field('jackett_url');
+        var key = Lampa.Storage.field('jackett_key');
+        var interview = Lampa.Storage.field('jackett_interview') == 'healthy' ? 'status:healthy' : 'all';
+
+        if (!url || !key) {
+            Lampa.Noty.show('Jackett не настроен');
+            return;
+        }
+
+        url = Lampa.Utils.checkEmptyUrl(url);
+
+        var u = url + '/api/v2.0/indexers/' + interview + '/results?apikey=' + key + '&Query=' + encodeURIComponent(query);
+
+        if (movie) {
+            u = Lampa.Utils.addUrlComponent(u, 'title=' + encodeURIComponent(movie.title));
+            u = Lampa.Utils.addUrlComponent(u, 'title_original=' + encodeURIComponent(movie.original_title || movie.original_name));
+            
+            var is_serial = movie.original_name ? '2' : '1';
+            u = Lampa.Utils.addUrlComponent(u, 'is_serial=' + is_serial);
+
+            if (movie.genres) {
+                var genres = movie.genres.map(function(a) { return a.name; }).join(',');
+                u = Lampa.Utils.addUrlComponent(u, 'genres=' + encodeURIComponent(genres));
+            }
+
+            var cat = (movie.number_of_seasons > 0 ? 5000 : 2000) + (movie.original_language == 'ja' ? ',5070' : '');
+            u = Lampa.Utils.addUrlComponent(u, 'Category[]=' + cat);
+        }
+
+        u = Lampa.Utils.addUrlComponent(u, 'force_search=true');
+
+        console.log('Force Search URL:', u);
+        Lampa.Noty.show('Начат принудительный поиск');
+
+        $.ajax({
+            url: u,
+            type: 'GET',
+            success: function() {
+                // Перезагружаем активность для обновления списка
+                if (activity.component === 'torrents') {
+                    Lampa.Activity.replace({
+                        component: 'torrents',
+                        movie: movie,
+                        search: query,
+                        page: 1
+                    });
+                }
+            },
+            error: function() {
+                Lampa.Noty.show('Ошибка запроса Force Search');
             }
         });
     }
+
 })();
