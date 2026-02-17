66c53bde3b34ab63b888daa1e4c4bf38d3ae803a
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-02-17 15:47:32 +0300

    Update fix_search.js

--- fix_search.js
+++ fix_search.js
@@ -1,64 +1,105 @@
 (function () {
     'use strict';
 
-    // Добавляем настройку
-    /*
-    Lampa.Settings.listener.follow('open', function (e) {
-        if (e.name == 'parser') {
-            var item = $(`
-                <div class="settings-param selector" data-type="toggle" data-name="parser_use_tmdb_id">
-                    <div class="settings-param__name">Поиск по TMDB ID</div>
-                    <div class="settings-param__value"></div>
-                    <div class="settings-param__descr">Добавлять ID TMDB к запросу поиска торрентов</div>
-                </div>
-            `);
-
-            var target = e.body.find('[data-name="parser_torrent_type"]');
-            if (target.length) {
-                target.after(item);
-            } else {
-                e.body.find('.settings-param').first().before(item);
-            }
-
-            Lampa.Params.bind(item);
-        }
-    });
-    */
-
     Lampa.Listener.follow('request_before', function (e) {
-        // Проверяем, что это запрос к Jackett
         if (e.params.url && e.params.url.indexOf('/api/v2.0/indexers/') !== -1 && e.params.url.indexOf('/results') !== -1) {
-            
-            // Убираем параметр year из запроса
             e.params.url = e.params.url.replace(/([?&])year=[^&]*&?/, '$1').replace(/&$/, '');
-
-            /*
-            // Логика добавления TMDB ID
-            if (Lampa.Storage.field('parser_use_tmdb_id')) {
-                var activity = Lampa.Activity.active();
-                var movie = null;
-
-                if (activity.component === 'full' && activity.card) movie = activity.card;
-                if (activity.component === 'torrents' && activity.movie) movie = activity.movie;
-
-                if (movie && movie.id) {
-                    var match = e.params.url.match(/title_original=([^&]+)/);
-                    
-                    if (match && match[1]) {
-                        var urlTitle = decodeURIComponent(match[1]).toLowerCase();
-                        var cardTitle = (movie.original_title || movie.original_name || '').toLowerCase();
-
-                        if (urlTitle === cardTitle || cardTitle.indexOf(urlTitle) > -1 || urlTitle.indexOf(cardTitle) > -1) {
-                            e.params.url = Lampa.Utils.addUrlComponent(e.params.url, 'tmdb=' + movie.id);
-                            e.params.url = Lampa.Utils.addUrlComponent(e.params.url, 'tmdb-search=true');
-                        }
-                    } else {
-                        e.params.url = Lampa.Utils.addUrlComponent(e.params.url, 'tmdb=' + movie.id);
-                        e.params.url = Lampa.Utils.addUrlComponent(e.params.url, 'tmdb-search=true');
-                    }
-                }
-            }
-            */
         }
     });
+
+    Lampa.Listener.follow('activity', function (e) {
+        if (e.type === 'start' && e.component === 'torrents') {
+            var waitFilter = setInterval(function(){
+                var filter = $('.torrent-filter');
+                if (filter.length) {
+                    clearInterval(waitFilter);
+                    
+                    if (filter.find('.filter--reload').length) return;
+
+                    var btn = $(`
+                        <div class="simple-button simple-button--filter selector filter--reload">
+                            <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="width: 1.5em; height: 1.5em;">
+                                <path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z" fill="currentColor"></path>
+                            </svg>
+                        </div>
+                    `);
+
+                    filter.find('.filter--sort').after(btn);
+
+                    btn.on('hover:enter', function () {
+                        forceSearch();
+                    });
+                }
+            }, 200);
+        }
+    });
+
+    function forceSearch() {
+        var activity = Lampa.Activity.active();
+        var movie = activity.movie;
+        var query = activity.search;
+
+        var useLink = Lampa.Storage.field('parser_use_link');
+        var url, key;
+
+        if (useLink == 'two') {
+            url = Lampa.Storage.field('jackett_url_two');
+            key = Lampa.Storage.field('jackett_key_two');
+        } else {
+            url = Lampa.Storage.field('jackett_url');
+            key = Lampa.Storage.field('jackett_key');
+        }
+
+        var interview = Lampa.Storage.field('jackett_interview') == 'healthy' ? 'status:healthy' : 'all';
+
+        if (!url || !key) {
+            Lampa.Noty.show('Jackett не настроен');
+            return;
+        }
+
+        url = Lampa.Utils.checkEmptyUrl(url);
+
+        var u = url + '/api/v2.0/indexers/' + interview + '/results?apikey=' + key + '&Query=' + encodeURIComponent(query);
+
+        if (movie) {
+            try {
+                u += '&title=' + encodeURIComponent(movie.title);
+                u += '&title_original=' + encodeURIComponent(movie.original_title || movie.original_name);
+                
+                var is_serial = (movie.original_name || movie.number_of_seasons > 0) ? '2' : '1';
+                u += '&is_serial=' + is_serial;
+
+                if (movie.genres) {
+                    var genres = movie.genres.map(function(a) { return a.name; }).join(',');
+                    u += '&genres=' + encodeURIComponent(genres);
+                }
+
+                var cat = (movie.number_of_seasons > 0 ? 5000 : 2000) + (movie.original_language == 'ja' ? ',5070' : '');
+                u += '&Category[]=' + cat;
+            } catch (e) {}
+        }
+
+        u += '&force_search=true';
+
+        Lampa.Noty.show('Начат принудительный поиск');
+
+        $.ajax({
+            url: u,
+            type: 'GET',
+            success: function() {
+                if (activity.component === 'torrents') {
+                    Lampa.Activity.replace({
+                        component: 'torrents',
+                        movie: movie,
+                        search: query,
+                        page: 1
+                    });
+                }
+            },
+            error: function() {
+                Lampa.Noty.show('Ошибка запроса Force Search');
+            }
+        });
+    }
+
 })();
